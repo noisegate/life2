0dc7a5cce3475d8f4fbae117e5d160887af3311b
xxxpokemon <c.marosvolgyi@gmail.com>
2017-11-08 23:52:42 +0100

    modified:   life2.cpp 	modified:   life2.h

--- life2.cpp
+++ life2.cpp
@@ -1,7 +1,3 @@
-#include "life2.h"
-#include <cmath>
-#include <cstdlib>
-#include <iostream>
 /*
  _     _  __
 | |   (_)/ _| ___
@@ -10,12 +6,56 @@
 |_____|_|_|  \___|
 
 */
+#include <cmath>
+#include <cstdlib>
+#include <iostream>
 
+#define MAX 1000
 #define MAXCIJFERS 20
-
 using namespace std;
 
+
+void lees_Optie (char & );
+void lees_Getal (int &, int);
+void random_Getal ();
+void menu (char & , int &);
+void sub_Menu ();
+
+class Life {
+  private:
+    bool wereld[MAX][MAX];
+    bool hulpwereld[MAX][MAX];
+    int hoogte, breedte, hoeki, hoekj;
+    int schuif, percentage, generaties;
+  public:
+    Life (); //constructor
+    void menu();
+    void drukaf ();
+    void vulrandom ();
+    void maakschoon ();
+    void zetpercentage (int& getal);
+    void gaan ();
+};
+
+Life::Life(){
+  hoogte = 20;
+  breedte = 30;
+  hoeki = 3;
+  hoekj = 3;
+}
+
+//stel percentage in tussen 0 en 100
+void Life::zetpercentage(int& getal){
+  lees_Getal(getal, 100);
+  cout << "Nieuwe waarde.. " << endl;
+  cin >> getal;
+}
+
+
 int main() {
+
+  Life mylife;
+
   char kies; // kiezen van C,R,P of S
   int getal;
 
@@ -38,7 +78,7 @@ void menu(char &kies, int &getal) {
     break;
   case 'r': // random
   case 'R': // random
-    cout << "Tekstje op scherm." << endl;
+    random_Getal();
     break;
   case 'p': // submenu
   case 'P': // submenu
@@ -56,17 +96,28 @@ void menu(char &kies, int &getal) {
 // SUBMENU
 void sub_Menu() {
   char kies;
-  cout << "(T)erug" << endl;
+  bool doen=true;
+  cout << "(T)erug (P)ercentage" << endl;
   lees_Optie(kies);
-  switch (kies) {
-  case 't':
-    break;
-  case 'T':
-    break;
-  }
+  while(doen){
+    switch (kies) {
+    case 't': //terug
+    case 'T': //terug
+      doen=false;
+      break;
+    case 'p': //percentage
+    case 'P': //percentage
+      //mylife.zetpercentage();
+      doen=false;
+      break;
+    default:
+      //sub_Menu(Life &mylife);
+      break;
+    }
+  }//while
 }
 // LEESOPTIE
-void lees_Optie(char &kies) {
+void lees_Optie(char& kies) {
   char weg = 'a';
   kies = '\n';
   do {
@@ -77,7 +128,7 @@ void lees_Optie(char &kies) {
   } while (weg != '\n');
 }
 // LEESGETAL
-void lees_Getal(int &getal, int maximum) {
+void lees_Getal(int& getal, int maximum) {
   char dummy;
   char myarray[MAXCIJFERS];
   int teller = 0;
@@ -109,3 +160,12 @@ void lees_Getal(int &getal, int maximum) {
 
   getal = dummygetal;
 }
+// RANDOMGENERATOR
+// SHEET 10
+// geef random getal tussen 0 en 999
+void random_Getal() {
+  static int getal = 42;
+  getal = (221 * getal + 1) % 1000; // niet aan knoeien
+  //return getal;
+  cout << 1 + rand() % 11 << endl;
+} // randomgetal
--- life2.h
+++
@@ -1,5 +0,0 @@
-
-void menu (char & , int &);
-void sub_Menu ();
-void lees_Optie (char & );
-void lees_Getal (int &, int);
