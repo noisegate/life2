645ba16898b8044f95fc96908c35c7eb666e93fa
xxxpokemon <c.marosvolgyi@gmail.com>
2017-11-09 00:44:18 +0100

    modified:   life2.cpp

--- life2.cpp
+++ life2.cpp
@@ -27,14 +27,23 @@ class Life {
     bool hulpwereld[MAX][MAX];
     int hoogte, breedte, hoeki, hoekj;
     int schuif, percentage, generaties;
+    char kies;
+    int getal;
   public:
     Life (); //constructor
     void menu();
+    void sub_Menu();
+    void lees_Optie();
+    void lees_Getal();
+    int random_Getal();
     void drukaf ();
     void vulrandom ();
     void maakschoon ();
-    void zetpercentage (int& getal);
+    void zetpercentage ();
     void gaan ();
+    void start();
+    void heelschoon();
+    void schoon();
 };
 
 Life::Life(){
@@ -44,33 +53,38 @@ Life::Life(){
   hoekj = 3;
 }
 
-//stel percentage in tussen 0 en 100
-void Life::zetpercentage(int& getal){
-  lees_Getal(getal, 100);
-  cout << "Nieuwe waarde.. " << endl;
-  cin >> getal;
+void Life::heelschoon(){
+  for (int i=0;i<MAX;i++)
+    for (int j=0;j<MAX;j++)
+      wereld[i][j]=false;
+
 }
 
-
-int main() {
-
-  Life mylife;
-
-  char kies; // kiezen van C,R,P of S
-  int getal;
-
-  kies = 'a'; // kies wordt gebruikt in het menu
-
-  while (kies != 's' && kies != 'S') {
-    menu(kies, getal);
+void Life::drukaf(){
+  for(int y=0; y<hoogte;y++){
+    for (int x=0; x<breedte;x++)
+      cout << (wereld[y+hoekj][x+hoeki]?'x':' ');
+    cout << endl;
   }
-
-  return 0;
 }
-// HOOFDMENU
-void menu(char &kies, int &getal) {
+
+//stel percentage in tussen 0 en 100
+void Life::zetpercentage(){
+  cout << "Nieuwe waarde.. " << endl;
+  lees_Getal();
+  //cin >> getal;
+}
+
+void Life::start(){
+  kies = 'a';
+  while (kies != 's' && kies != 'S') {
+    menu();
+  }
+}
+
+void Life::menu() {
   cout << "s(C)hoon (R)andom (P)arameters (S)toppen" << endl;
-  lees_Optie(kies);
+  lees_Optie();
   switch (kies) {
   case 'c': // schoon
   case 'C': // schoon
@@ -78,14 +92,17 @@ void menu(char &kies, int &getal) {
     break;
   case 'r': // random
   case 'R': // random
-    random_Getal();
+    vulrandom();
     break;
   case 'p': // submenu
   case 'P': // submenu
     sub_Menu();
     break;
+  case 'd':
+    //heelschoon();
+    drukaf();
   case 't':
-    lees_Getal(getal, 1000);
+    lees_Getal();
     cout << getal << endl;
     break;
   case 's': // stoppen
@@ -93,12 +110,13 @@ void menu(char &kies, int &getal) {
     break;
   }
 }
+
 // SUBMENU
-void sub_Menu() {
-  char kies;
+void Life::sub_Menu() {
+
   bool doen=true;
   cout << "(T)erug (P)ercentage" << endl;
-  lees_Optie(kies);
+  lees_Optie();
   while(doen){
     switch (kies) {
     case 't': //terug
@@ -107,8 +125,9 @@ void sub_Menu() {
       break;
     case 'p': //percentage
     case 'P': //percentage
-      //mylife.zetpercentage();
+      zetpercentage();
       doen=false;
+      cout << getal;
       break;
     default:
       //sub_Menu(Life &mylife);
@@ -116,8 +135,15 @@ void sub_Menu() {
     }
   }//while
 }
+
+void Life::vulrandom(){
+  for (int x = hoeki; x<(hoeki+breedte); x++)
+    for (int y = hoekj; y<(hoekj+hoogte); y++)
+      wereld[y][x]=(random_Getal()<500?false:true);
+}
+
 // LEESOPTIE
-void lees_Optie(char& kies) {
+void Life::lees_Optie() {
   char weg = 'a';
   kies = '\n';
   do {
@@ -128,7 +154,7 @@ void lees_Optie(char& kies) {
   } while (weg != '\n');
 }
 // LEESGETAL
-void lees_Getal(int& getal, int maximum) {
+void Life::lees_Getal() {
   char dummy;
   char myarray[MAXCIJFERS];
   int teller = 0;
@@ -160,12 +186,24 @@ void lees_Getal(int& getal, int maximum) {
 
   getal = dummygetal;
 }
+
 // RANDOMGENERATOR
 // SHEET 10
 // geef random getal tussen 0 en 999
-void random_Getal() {
-  static int getal = 42;
-  getal = (221 * getal + 1) % 1000; // niet aan knoeien
-  //return getal;
-  cout << 1 + rand() % 11 << endl;
+int Life::random_Getal() {
+  static int _getal = 42;
+  _getal = (221 * _getal + 1) % 1000; // niet aan knoeien
+  return _getal;
+
 } // randomgetal
+
+int main() {
+
+  Life mylife;
+
+  mylife.heelschoon();
+  mylife.start();
+
+  return 0;
+}
+// HOOFDMENU
